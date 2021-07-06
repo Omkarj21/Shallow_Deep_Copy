15c02820e3081f7e3437483807850ffbc389cc66
Omkar Jadhav <42997414+Omkarj21@users.noreply.github.com>
2021-07-06 23:38:52 +0530

    Update shallow_copy.py

--- shallow_copy.py
+++ shallow_copy.py
@@ -10,29 +10,29 @@ print("Copied List : ",copy_list)		# Output : [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 # result/output of below 2 lines will not be same as it is creating new object but nested objects will
 # will not be created, it will share reference of original nested objects
-print("Id of Original List : ",id(original_list))
-print("Id of Copied List : ",id(copy_list))
+print("Id of Original List : ",id(original_list))  # Id of Original List :  1563020705864
+print("Id of Copied List : ",id(copy_list))        # Id of Copied List   :  1563020707272
 
 # result/output of below 2 lines will be same as it is sharing the reference of nested elements of original object
 # output of nested 3 elements will be same as when this step "copy.copy(original_list)" executed ,
 # it was having 3 nested elements
-print("Id of Original List 0th element : ",id(original_list[0]))
-print("Id of Copied List 0th element  : ",id(copy_list[0]))
+print("Id of Original List 0th element : ",id(original_list[0])) # Id of Original List 0th element :  1563020678984
+print("Id of Copied List 0th element  : ",id(copy_list[0]))      # Id of Copied List 0th element   :  1563020678984
 
 original_list.append(["a","b","c"])
-print("Original List after append in original_list : ",original_list)		#Output : [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
-print("Copied List after append in original_list : ",copy_list)		#Output : [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]
+print("Original List after append in original_list : ",original_list)		#Output : [[1, 2, 3], [4, 5, 6], [7, 8, 9], ['a', 'b', 'c']]
+print("Copied List after append in original_list : ",copy_list)		#Output : [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 copy_list.append(["p","q","r"])
-print("Original List after append in copy_list : ",original_list)		#Output : [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15]]
-print("Copied List after append in copy_list : ",copy_list)		#Output : [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12], [13, 14, 15]]
+print("Original List after append in copy_list : ",original_list)		#Output : [[1, 2, 3], [4, 5, 6], [7, 8, 9], ['a', 'b', 'c']]
+print("Copied List after append in copy_list : ",copy_list)		#Output : [[1, 2, 3], [4, 5, 6], [7, 8, 9], ['p', 'q', 'r']]
 
 # result/output of below 2 lines will not be same as it is not sharing the reference of nested elements of
 # original object, So output of only nested 3 elements will be same as when this step "copy.copy(original_list)" executed ,
 # it was having 3 nested elements, after 3rd nested elements every newly created nested element will
 # have different reference.
-print("Id of Original List 3th element : ",id(original_list[3]))
-print("Id of Copied List 3th element  : ",id(copy_list[3]))
+print("Id of Original List 3th element : ",id(original_list[3])) # Id of Original List 3th element :  2304153175752
+print("Id of Copied List 3th element  : ",id(copy_list[3]))      # Id of Copied List 3th element   :  2304153175432
 
 # Value of "D" will be added to both Original & Copied List, because of above reason : line 30 to 33
 copy_list[1][1]="D"
